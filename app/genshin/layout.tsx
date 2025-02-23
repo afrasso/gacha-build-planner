7dcb970f0d24b7d26bfa371718bf9a7d21332d71
Anthony Frasso <anf4@cornell.edu>
2025-02-23 16:31:31 -0500

    Fixing casing.

--- app/genshin/layout.tsx
+++ app/genshin/layout.tsx
@@ -7,7 +7,7 @@ import { loadYaml } from "@/utils/yamlhelper";
 
 const characters: Character[] = loadYaml<Character[]>(path.join(__datadir, "genshin", "characters.yaml"));
 const weapons: Weapon[] = loadYaml<Weapon[]>(path.join(__datadir, "genshin", "weapons.yaml"));
-const artifactSets: ArtifactSet[] = loadYaml<ArtifactSet[]>(path.join(__datadir, "genshin", "artifactSets.yaml"));
+const artifactSets: ArtifactSet[] = loadYaml<ArtifactSet[]>(path.join(__datadir, "genshin", "artifactsets.yaml"));
 
 export default function RootLayout({
   children,
