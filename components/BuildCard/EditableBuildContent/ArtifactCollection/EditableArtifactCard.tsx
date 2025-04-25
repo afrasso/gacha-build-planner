e5c3883aa3623a818cba74e6a48c1f5233031f6e
Anthony Frasso <anf4@cornell.edu>
2025-04-25 18:16:56 -0400

    Fix Firefox dialog issue (#30)

--- components/BuildCard/EditableBuildContent/ArtifactCollection/EditableArtifactCard.tsx
+++ components/BuildCard/EditableBuildContent/ArtifactCollection/EditableArtifactCard.tsx
@@ -23,23 +23,36 @@ const EditableArtifactCard: React.FC<EditableArtifactCardProps> = ({ artifact, a
   };
 
   return (
-    <Dialog key={artifactTypeKey} onOpenChange={setIsDialogOpen} open={isDialogOpen}>
-      <ArtifactCard
-        artifact={artifact}
-        artifactTypeKey={artifactTypeKey}
-        onEdit={() => setIsDialogOpen(true)}
-        showInfoButton={true}
-        showMetrics={false}
-      />
-      <DialogContent className="sm:max-w-[425px]">
-        <DialogHeader>
-          <DialogTitle>
-            {artifact ? "Edit" : "Add"} {artifactTypeKey} Artifact
-          </DialogTitle>
-        </DialogHeader>
-        <ArtifactEditor artifact={artifact} artifactTypeKey={artifactTypeKey} onUpdate={update} />
-      </DialogContent>
-    </Dialog>
+    <>
+      {/* This overlay blocks background clicks, since the having modal={true} is broken in Firefox. See
+      https://github.com/radix-ui/primitives/issues/2390. It can be deleted once that issue is solved. */}
+      {isDialogOpen && <div aria-hidden="true" className="fixed inset-0 bg-black/50 z-40" />}
+
+      <Dialog key={artifactTypeKey} modal={false} onOpenChange={setIsDialogOpen} open={isDialogOpen}>
+        <ArtifactCard
+          artifact={artifact}
+          artifactTypeKey={artifactTypeKey}
+          onEdit={() => setIsDialogOpen(true)}
+          showInfoButton={true}
+          showMetrics={false}
+        />
+        <DialogContent
+          className="sm:max-w-[425px]"
+          // This prevents the dialog from closing when they click the overlay. Not needed when the modal flag is
+          // removed once the fix above is available.
+          onInteractOutside={(event) => {
+            event.preventDefault();
+          }}
+        >
+          <DialogHeader>
+            <DialogTitle>
+              {artifact ? "Edit" : "Add"} {artifactTypeKey} Artifact
+            </DialogTitle>
+          </DialogHeader>
+          <ArtifactEditor artifact={artifact} artifactTypeKey={artifactTypeKey} onUpdate={update} />
+        </DialogContent>
+      </Dialog>
+    </>
   );
 };
 
